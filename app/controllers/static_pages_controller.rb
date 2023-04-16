042fe3dda539229455fb1352092a76449a50825b
kzog14 <130205983+kzog14@users.noreply.github.com>
2023-04-16 13:09:37 +0000

    Exec: rails generate controller StaticPages home help

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+    # render ....
+    # => app/views/static_pages/home.html.erb
+  end
+
+  def help
+    # => app/views/static_pages/help.html.erb
+  end
+end
