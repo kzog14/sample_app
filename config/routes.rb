ce3715850afd5af06dfd8c187f70d773dd8eae8d
kzog14 <130205983+kzog14@users.noreply.github.com>
2023-05-09 11:48:19 +0000

    Finish ch08

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
  
   # GET / => static_pages#home
   root 'static_pages#home'
@@ -9,10 +10,12 @@ Rails.application.routes.draw do
   # get  'static_pages/about'
   # get  "static_pages/contact"
   # get 'users/new'
-  get  "/help",    to: "static_pages#help"
-  get  "/about",   to: "static_pages#about"
-  get  "/contact", to: "static_pages#contact"
-  get  "/signup",  to: "users#new"
+  get    "/help",    to: "static_pages#help"
+  get    "/about",   to: "static_pages#about"
+  get    "/contact", to: "static_pages#contact"
+  get    "/signup",  to: "users#new"
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
   resources :users
-
 end
